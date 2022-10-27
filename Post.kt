f897070ed13dad0f2603281001c24edec19beee2
Tyler Benjamin <94323045+TyGBenjamin@users.noreply.github.com>
2022-10-27 14:12:54 -0400

    Add files via upload

---
+++ Post.kt
@@ -0,0 +1,51 @@
+package com.example.twittertest.presentation.screens.components
+
+//@Composable
+//fun Post(
+//    data: Post,
+//    navController: NavController
+//) {
+//    Card(
+//        modifier = Modifier
+//            .fillMaxWidth()
+//            .clip(RoundedCornerShape(4.dp))
+//
+//    ) {
+//        Row(modifier = Modifier.height(intrinsicSize = IntrinsicSize.Max))
+//        {
+//            Image(
+//                painter = rememberAsyncImagePainter(
+//                    "https://image.shutterstock.com/image-vector/one-piece-character-luffy-nika-600w-2210725005.jpg"
+//                ),
+//                contentDescription = null,
+//                modifier = Modifier.size(40.dp)
+//            )
+//            Column {
+//
+//
+//                Text(text = "Username here")
+//                Text(
+//                    text = "Username here two",
+//                    modifier = Modifier.padding(vertical = 20.dp)
+//                )
+//                Text(text = "comments", fontSize = 15.sp)
+//
+//
+//            }
+//            Text(text = "Time", textAlign = TextAlign.End)
+//
+//        }
+//    }
+//}
+
+
+/**
+ *
+ */
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview12() {
+//    TwitterTestTheme {
+//        Post(data, navController)
+//    }
+//}
